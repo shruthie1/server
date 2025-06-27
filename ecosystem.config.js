7cff514cbfa00705a7510c9f4d2a5143c3cb476a
SSK <sheetysai>
2025-06-27 15:51:45 +0530

    Done

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "300M",
-      cron_restart: "0 1 * * *",
+      cron_restart: "5 1 * * *",
       restart_delay: 5000,
       namespace: "clients"
     },
@@ -35,7 +35,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "300M",
-      cron_restart: "0 1 * * *",
+      cron_restart: "10 1 * * *",
       restart_delay: 5000,
       namespace: "clients"
     },
@@ -48,7 +48,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "300M",
-      cron_restart: "0 1 * * *",
+      cron_restart: "15 1 * * *",
       restart_delay: 5000,
       namespace: "clients"
     },
@@ -61,7 +61,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "300M",
-      cron_restart: "0 1 * * *",
+      cron_restart: "20 1 * * *",
       restart_delay: 5000,
       namespace: "clients"
     },
@@ -74,7 +74,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "300M",
-      cron_restart: "0 1 * * *",
+      cron_restart: "25 1 * * *",
       restart_delay: 5000,
       namespace: "clients"
     },
@@ -87,7 +87,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "300M",
-      cron_restart: "0 1 * * *",
+      cron_restart: "30 1 * * *",
       restart_delay: 5000,
       namespace: "clients"
     },
@@ -100,7 +100,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "300M",
-      cron_restart: "0 1 * * *",
+      cron_restart: "35 1 * * *",
       restart_delay: 5000,
       namespace: "clients"
     },
@@ -113,7 +113,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "300M",
-      cron_restart: "0 1 * * *",
+      cron_restart: "40 1 * * *",
       restart_delay: 5000,
       namespace: "clients"
     },
@@ -126,7 +126,137 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "300M",
-      cron_restart: "0 1 * * *",
+      cron_restart: "45 1 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "ramya1",
+      script: "server.js",
+      env: {
+        PORT: 4011,
+        clientId: "ramya1"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "50 1 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "ramya2",
+      script: "server.js",
+      env: {
+        PORT: 4012,
+        clientId: "ramya2"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "55 1 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "divya1",
+      script: "server.js",
+      env: {
+        PORT: 4013,
+        clientId: "divya1"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "0 2 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "divya2",
+      script: "server.js",
+      env: {
+        PORT: 4014,
+        clientId: "divya2"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "5 2 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "sowmya1",
+      script: "server.js",
+      env: {
+        PORT: 4015,
+        clientId: "sowmya1"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "10 2 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "sowmya2",
+      script: "server.js",
+      env: {
+        PORT: 4016,
+        clientId: "sowmya2"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "15 2 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "sneha1",
+      script: "server.js",
+      env: {
+        PORT: 4017,
+        clientId: "sneha1"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "20 2 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "sneha2",
+      script: "server.js",
+      env: {
+        PORT: 4018,
+        clientId: "sneha2"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "25 2 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "nidhi1",
+      script: "server.js",
+      env: {
+        PORT: 4019,
+        clientId: "nidhi1"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "30 2 * * *",
+      restart_delay: 5000,
+      namespace: "clients"
+    },
+    {
+      name: "nidhi2",
+      script: "server.js",
+      env: {
+        PORT: 4020,
+        clientId: "nidhi2"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "35 2 * * *",
       restart_delay: 5000,
       namespace: "clients"
     }
