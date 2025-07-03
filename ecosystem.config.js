62ea2b01a271c689b1faa3262e2d01aa664ee20a
SSK <sheetysai>
2025-07-03 18:32:11 +0530

    okayish

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ const generateRandomCron = (index) => {
   const hour = Math.floor(Math.random() * 8) + (8 * (index % 3));
   return `${minute} ${hour}/8 * * *`;
 };
+
 module.exports = {
   apps: [
     {
@@ -15,7 +16,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      cron_restart: generateRandomCron(0), //"30 19 * * *", // 1:00 AM IST (UTC 19:30)
+      cron_restart: generateRandomCron(0),
       restart_delay: 5000,
       namespace: "promotions",
       kill_timeout: 5000,
@@ -33,7 +34,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      cron_restart: generateRandomCron(1), //"40 19 * * *", // 1:10 AM IST (UTC 19:40)
+      cron_restart: generateRandomCron(1),
       restart_delay: 5000,
       namespace: "promotions",
       kill_timeout: 5000,
@@ -51,7 +52,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      cron_restart: generateRandomCron(2), //"50 19 * * *", // 1:20 AM IST (UTC 19:50)
+      cron_restart: generateRandomCron(2),
       restart_delay: 5000,
       namespace: "promotions",
       kill_timeout: 5000,
@@ -69,7 +70,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      cron_restart: generateRandomCron(3), //"0 20 * * *", // 1:30 AM IST (UTC 20:00)
+      cron_restart: generateRandomCron(3),
       restart_delay: 5000,
       namespace: "promotions",
       kill_timeout: 5000,
@@ -87,7 +88,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      cron_restart: generateRandomCron(4), //"10 20 * * *", // 1:40 AM IST (UTC 20:10)
+      cron_restart: generateRandomCron(4),
       restart_delay: 5000,
       namespace: "promotions",
       kill_timeout: 5000,
@@ -105,7 +106,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      cron_restart: generateRandomCron(5), //"20 20 * * *", // 1:50 AM IST (UTC 20:20)
+      cron_restart: generateRandomCron(5),
       restart_delay: 5000,
       namespace: "promotions",
       kill_timeout: 5000,
@@ -123,7 +124,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      cron_restart: generateRandomCron(7), //"40 20 * * *", // 2:10 AM IST (UTC 20:40)
+      cron_restart: generateRandomCron(6),
       restart_delay: 5000,
       namespace: "promotions",
       kill_timeout: 5000,
@@ -141,7 +142,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      generateRandomCron: generateRandomCron(), //: "10 4 * * *",
+      cron_restart: generateRandomCron(7),
       restart_delay: 5000,
       kill_timeout: 3000,
       namespace: "promotions"
@@ -156,7 +157,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      generateRandomCron: generateRandomCron(), //: "20 4 * * *",
+      cron_restart: generateRandomCron(8),
       restart_delay: 5000,
       namespace: "promotions",
       kill_timeout: 5000,
@@ -174,7 +175,7 @@ module.exports = {
       },
       autorestart: true,
       max_memory_restart: "450M",
-      generateRandomCron: generateRandomCron(), //: "30 4 * * *",
+      cron_restart: generateRandomCron(9),
       restart_delay: 5000,
       namespace: "promotions",
       kill_timeout: 5000,
@@ -184,4 +185,3 @@ module.exports = {
     }
   ]
 };
-
