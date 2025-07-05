6b990838e959e7a7c64526cca6ec82fd8fe1e229
SSK <sheetysai>
2025-07-05 10:28:29 +0530

    done

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,19 @@ module.exports = {
       cron_restart: "0 2 * * *",
       restart_delay: 5000,
       namespace: "helpers"
+    },
+    {
+      name: "ums-test",
+      script: "server.js",
+      env: {
+        PORT: 5002,
+        clientId: "ums-test"
+      },
+      autorestart: true,
+      max_memory_restart: "300M",
+      cron_restart: "0 2 * * *",
+      restart_delay: 5000,
+      namespace: "helpers"
     }
   ]
 };
