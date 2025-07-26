18f2f232dacb53dc4a2270c149447a3c3a2d4099
ssk <dodieajt@gmail.com>
2025-07-26 12:38:30 +0530

    uu

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,10 +14,11 @@ const commonConfig = {
   namespace: "clients",
   kill_timeout: 5000,
   time: true,
-  log_date_format: "DD/MM/YYYY HH:mm:ss",
+  log_date_format: "DD/MM HH:mm:ss",
   merge_logs: true,
   env: {
     TZ: "Asia/Kolkata",
+    "log-date-format": "DD/MM HH:mm:ss",
   },
   min_uptime: "10s"
 };
